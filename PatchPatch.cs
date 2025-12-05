c9096733197a68dc0a4779d30754a5d9e5eedb47
Tom Holmes <tholmes150@gmail.com>
2025-12-05 11:11:52 -0600

    Initial release

---
+++ PatchPatch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Assets.Scripts;
+using Assets.Scripts.Serialization;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace PathPatch
+{
+  [BepInPlugin("pathpatch", "Path Patch", "0.1.0")]
+  class PathPatchMod : BaseUnityPlugin
+  {
+    public static ConfigEntry<bool> LogToConsole;
+
+    public void Awake()
+    {
+      if (Harmony.HasAnyPatches("PathPatch"))
+        return;
+
+      LogToConsole = Config.Bind(
+        new("Logging", "LogToConsole"),
+        false,
+        new("When enabled, logs directory delete errors to the in-game console")
+      );
+
+      var harmony = new Harmony("PathPatch");
+      harmony.PatchAll();
+    }
+  }
+
+  [HarmonyPatch]
+  static class DebugPatches
+  {
+    [HarmonyTargetMethod]
+    static MethodInfo TargetMethod()
+    {
+      var method = typeof(LoadHelper).GetMethod("LoadWorldTask", BindingFlags.Static | BindingFlags.NonPublic);
+      var attr = method.GetCustomAttribute<AsyncStateMachineAttribute>();
+      return attr.StateMachineType.GetMethod("MoveNext", BindingFlags.Instance | BindingFlags.NonPublic);
+    }
+
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> LoadHelper_LoadWorldTask(IEnumerable<CodeInstruction> instructions)
+    {
+      var matcher = new CodeMatcher(instructions);
+      matcher.MatchStartForward(CodeInstruction.Call(() => Directory.Delete("", true)));
+      if (matcher.IsInvalid)
+        throw new Exception("could not find LoadWorldTask insertion point");
+
+      matcher.RemoveInstruction();
+      matcher.InsertAndAdvance(CodeInstruction.Call(() => TryDelete(default, default)));
+
+      return matcher.Instructions();
+    }
+
+    static void LogError(Exception ex)
+    {
+      Debug.Log($"Ignoring delete error: {ex.Message}\n{ex.StackTrace}");
+      if (PathPatchMod.LogToConsole.Value)
+        ConsoleWindow.Print($"{ex.Message}\n{ex.StackTrace}", ConsoleColor.Red, aged: false);
+    }
+
+    static void TryDelete(string path, bool recurse)
+    {
+      try
+      {
+        Directory.Delete(path, recurse);
+      }
+      catch (Exception ex)
+      {
+        LogError(ex);
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Stationeers Path Patch
+
+When running Stationeers on OSX using CodeWeavers CrossOver, the game fails to delete the temp folder used to unzip save files, causing the load to fail. This mod catches that error and allows loading to continue, optionally printing the error to the in-game console.
+
+
+This requires an installed mod loader to run. See: https://github.com/StationeersLaunchPad/StationeersLaunchPad
+
+Download `PathPatch.zip` from Releases and unzip into `%HOME%/Documents/my games/Stationeers/mods/`
